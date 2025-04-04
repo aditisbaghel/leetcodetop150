75c6741a3e20d0a79e6954b0c7066726c60dd386
Aditi Singh <73370923+aditisbaghel@users.noreply.github.com>
2025-04-05 05:23:57 +0530

    Time: 8 ms (27.46%), Space: 56.1 MB (84.27%) - LeetHub

--- 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
@@ -1,27 +1,57 @@
 class Solution {
     public int waysToMakeFair(int[] nums) {
-    int leftOdd=0, leftEven =0, tEven=0, tOdd =0,count=0;
-    for(int i =0;i<nums.length; i++){
-        if(i%2==0){
-            tEven+=nums[i];
+        int oddSum = 0, evenSum=0, count=0;
+        int[] oddArray = new int[nums.length];
+        int[] evenArray = new int[nums.length];
+        for(int i =0;i<nums.length;i++){
+            if(i%2==0){
+                evenSum+= nums[i];
+            }
+            else{
+                oddSum+= nums[i];
+            }
+            oddArray[i] = oddSum;
+            evenArray[i] = evenSum;
         }
-        else{
-            tOdd+=nums[i];
+        for(int i = 0;i<nums.length;i++){
+            if(i==0){
+                oddSum = evenArray[nums.length-1]-evenArray[0];
+                evenSum = oddArray[nums.length-1]-oddArray[0];
+            } else if (i == nums.length - 1) {  
+                evenSum = evenArray[i - 1];
+                oddSum = oddArray[i - 1];
+            } 
+            else{
+            evenSum = evenArray[i-1] + (oddArray[nums.length - 1]-oddArray[i]);
+            oddSum = oddArray[i-1] + (evenArray[nums.length - 1]-evenArray[i]);
+            }
+             if (oddSum == evenSum) {
+                count++;
+            }
         }
-    }
-    for(int i =0;i<nums.length;i++){
-        if(i%2==0)
-        tEven-=nums[i];
-        else
-        tOdd-=nums[i];
-        if(leftOdd+tEven==leftEven+tOdd){
-            count++;
-        }
-        if(i%2==0)
-        leftEven+=nums[i];
-        else
-        leftOdd+=nums[i];
-    }
     return count;
+    // int leftOdd=0, leftEven =0, tEven=0, tOdd =0,count=0;
+    // for(int i =0;i<nums.length; i++){
+    //     if(i%2==0){
+    //         tEven+=nums[i];
+    //     }
+    //     else{
+    //         tOdd+=nums[i];
+    //     }
+    // }
+    // for(int i =0;i<nums.length;i++){
+    //     if(i%2==0)
+    //     tEven-=nums[i];
+    //     else
+    //     tOdd-=nums[i];
+    //     if(leftOdd+tEven==leftEven+tOdd){
+    //         count++;
+    //     }
+    //     if(i%2==0)
+    //     leftEven+=nums[i];
+    //     else
+    //     leftOdd+=nums[i];
+    // }
+    // return count;
     }
 }
